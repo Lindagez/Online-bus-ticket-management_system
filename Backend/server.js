4e1cc781468c8558cc699f9d918056a045b91dac
lidiya <lidiyagez@gmail.com>
2024-08-23 09:25:50 +0300

    implemented bus

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const port = process.env.PORT || 8080;
 
 
+const busRouter = require("./src/controllers/bus.controller");
 const userRouter=require("./src/controllers/user.controller");
+
 
 const connect = require("./src/configs/db");
 
@@ -12,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/user",userRouter)
-
+app.use("/bus", busRouter);
 
 
 
